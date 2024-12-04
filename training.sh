b98cdfe4cd0cf02ffe66b08080c12fa4e4e9907a
Lucas <lbometon@hotmail.fr>
2024-12-04 15:27:14 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -4,7 +4,7 @@ pip install --upgrade pip
 
 # Installe les dépendances nécessaires
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft accelerate bitsandbytes scikit-learn evaluate scipy joblib threadpoolctl
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0" unsloth peft accelerate bitsandbytes scikit-learn evaluate scipy joblib threadpoolctl
 
 # Exécute le script Python
 python training.py
