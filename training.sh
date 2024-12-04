ac7edc270a91e7e53b7a2e6ca81bfc30e8847566
Lucas <lbometon@hotmail.fr>
2024-12-04 15:43:13 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -7,15 +7,10 @@ source venv/bin/activate
 # Update pip
 pip install --upgrade pip
 
-# Clean existing installations
-pip uninstall -y unsloth
-pip uninstall -y transformers
-pip uninstall -y peft
-pip uninstall -y accelerate
-pip uninstall -y bitsandbytes
 
 # Install dependencies in the correct order
 pip install transformers>=4.37.2
+pip install unsloth-zoo
 pip install accelerate
 pip install bitsandbytes>=0.41.1
 pip install "unsloth[cuda] @ git+https://github.com/unslothai/unsloth.git"
