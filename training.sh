4f4e9cc1ac3b1b25115e4b10772b7ef26d9b6f8f
Lucas <lbometon@hotmail.fr>
2024-12-04 15:57:06 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-# Create and activate a virtual environment
-python -m venv venv
-source venv/bin/activate
-
-# Update pip
+# Met à jour pip
 pip install --upgrade pip
 
+# Installe les dépendances nécessaires
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
 
-# Install dependencies in the correct order
-pip install transformers>=4.37.2
-pip install unsloth-zoo
-pip install accelerate
-pip install bitsandbytes>=0.41.1
-pip install "unsloth[cuda] @ git+https://github.com/unslothai/unsloth.git"
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft
-pip install evaluate scikit-learn scipy joblib threadpoolctl
-
-# Execute the Python script
+# Exécute le script Python
 python training.py
